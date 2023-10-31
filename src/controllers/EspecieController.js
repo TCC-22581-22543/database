fb333434bdd86bb4ea191877f6f635fb09e6d21f
Erika Gomes <erikagomes1407@gmail.com>
2023-10-31 16:20:47 -0300

    adição do método de upload para imagens de espécies

--- src/controllers/EspecieController.js
+++ src/controllers/EspecieController.js
@@ -1,75 +1,62 @@
 import Especie from "../models/Especie.js";
 
 class EspecieController {
+  async read(req, res) {
+    const { id } = req.params;
 
-    async read(req, res){
-        const { id } = req.params;
+    const especie = await Especie.findById(id);
 
-        const especie = await Especie.findById(id); 
+    if (!especie) {
+      return res
+        .status(400)
+        .json({ message: "Especie não encontrada, tente novamente!" });
+    }
 
-        if(!especie){
-            return res
-            .status(400)
-            .json({ message: "Especie não encontrada, tente novamente!" });
-        }
+    return res.status(200).json({
+      nome: especie.nome,
+      nome_cientifico: especie.nome_cientifico,
+      classificacao: especie.classificacao,
+      tipo_alimentacao: especie.tipo_alimentacao,
+      bioma: especie.bioma,
+      image_url: especie.image_url,
+      evolucao: especie.evolucao,
+      situacao_atual: especie.situacao_atual,
+    });
+  }
 
-        return res.status(200).json({
-            nome: especie.nome,
-            nome_cientifico: especie.nome_cientifico,
-            classificacao: especie.classificacao,
-            tipo_alimentacao: especie.tipo_alimentacao,
-            bioma: especie.bioma,
-            image_url: especie.image_url,
-            evolucao:  especie.evolucao,
-            situacao_atual: especie.situacao_atual,
+  async returnAllEspecies(req, res) {
+    try {
+      const especies = await Especie.find();
+
+      return res.status(200).json({ especies });
+    } catch (error) {
+      return res.status(500).json({ message: "Erro ao buscar especie", error });
+    }
+  }
+
+  async updatedPicture(req, res) {
+    try {
+      if (req.file) {
+        const especieId = req.params.id;
+        const imageUrl = req.file.path;
+
+        // Atualize a URL da imagem no documento da espécie
+        const updatedEspecie = await Especie.findByIdAndUpdate(especieId, {
+          image_url: imageUrl,
         });
-    }
 
-    async returnAllEspecies(req, res) {
-        try {
-          const especies = await Especie.find();
-    
-          return res.status(200).json({ especies });
-        } catch (error) {
-          return res
-            .status(500)
-            .json({ message: "Erro ao buscar especie", error });
+        if (!updatedEspecie) {
+          return res.status(404).json({ message: "Espécie não encontrada" });
         }
+
+        return res.json({ message: "Upload de imagem bem-sucedido", imageUrl });
+      } else {
+        return res.status(400).json({ message: "Erro no upload de imagem" });
       }
-
-    /*async update(req, res){
-        const { id, title, text } = req.body;
-
-        const anotation = await Anotacao.findById(id); 
-
-        if(!anotation){
-            return res
-            .status(400)
-            .json({ message: "Anotação não encontrada!" });
-        }
-        
-        let updatedFields = {};
-
-        if(title) updatedFields.title = title;
-        if(text) updatedFields.text = text;
-
-        const updatedNotation = await userExist.update(updatedFields);
-
-        return res.status(200).json({ anotation: updatedNotation });
+    } catch (error) {
+      return res.status(500).json({ message: "Erro ao processar a imagem" });
     }
-
-    async delete(req, res){
-        const { id } = req.params;
-
-        const findId = await Anotacao.findById(id);
-
-        if(!findId){
-            return res.status(400).json({message: "Anotação não encontrada"});
-        }
-
-        await findId.deleteOne();
-    }*/
-    
+  }
 }
 
 export default new EspecieController();
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,7 +4,9 @@ import AuthController from "../controllers/AuthController.js";
 import EspecieController from "../controllers/EspecieController.js";
 import { Router } from "express";
 
+
 const routes = Router();
+import upload from '../config/multer.js';
 
 routes.post("/register", UserController.create);
 routes.get("/showUser/:id", UserController.show);
@@ -21,5 +23,6 @@ routes.delete("/deleteNotes/:id", AnotacaoController.delete);
 
 routes.get("/showAllEspecies", EspecieController.returnAllEspecies);
 routes.get("/readEspecies/:id", EspecieController.read);
+routes.post("/upload-image/:id", upload.single("file"),EspecieController.updatedPicture);
 
 export default routes;
