339e7d8e6f7b1131a6a0f77ccdfdbc1a0f6ab77c
ErikaGomess <109635107+ErikaGomess@users.noreply.github.com>
2023-11-12 17:06:04 -0300

    Update EspecieController.js

--- src/controllers/EspecieController.js
+++ src/controllers/EspecieController.js
@@ -1,29 +1,6 @@
 import Especie from "../models/Especie.js";
 
 class EspecieController {
-  async read(req, res) {
-    const { id } = req.params;
-
-    const especie = await Especie.findById(id);
-
-    if (!especie) {
-      return res
-        .status(400)
-        .json({ message: "Especie não encontrada, tente novamente!" });
-    }
-
-    return res.status(200).json({
-      nome: especie.nome,
-      nome_cientifico: especie.nome_cientifico,
-      classificacao: especie.classificacao,
-      tipo_alimentacao: especie.tipo_alimentacao,
-      bioma: especie.bioma,
-      image_url: especie.image_url,
-      evolucao: especie.evolucao,
-      situacao_atual: especie.situacao_atual,
-    });
-  }
-
   async returnAllEspecies(req, res) {
     try {
       const especies = await Especie.find();
@@ -32,6 +9,21 @@ class EspecieController {
     } catch (error) {
       return res.status(500).json({ message: "Erro ao buscar especie", error });
     }
+  }
+  async returnSpeciesById(req, res) {
+    try {
+      const { id } = req.params;
+
+      const specie = await Especie.findById(id);
+
+      if (!specie) {
+        return res
+          .status(400)
+          .json({ message: "Erro ao buscar especie", error });
+      }
+
+      return res.status(200).json({ specie });
+    } catch (error) {}
   }
 }
 
