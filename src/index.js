18521da58105f606795126f0bbd1d4a1b505e7ee
matheuschiarelli11 <matheus.hchiarelli@gmail.com>
2023-11-13 15:47:10 -0300

    sprint 4

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import dotenv from "dotenv";
 import { connect } from "mongoose";
 import routes from "./routes/routes.js";
 import cors from "cors";
+import userRoutes from "seuCaminho/UserRoutes"; // Certifique-se de ajustar o caminho para suas rotas de usuário
+import passwordResetRoutes from "seuCaminho/passwordReset"; // Certifique-se de ajustar o caminho para as rotas de recuperação de senha
 
 const app = express();
 app.use(cors());
 dotenv.config();
+
+app.use("/users");
+app.use("/password-reset", passwordResetRoutes);
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
